40e9d193d6f284521e454ab209596921678085a5
league <league@wts14-3.attlocal.net>
2022-10-01 13:04:21 -0700

    star(in progress),pentagon,spiral,square

--- src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
+++ src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
@@ -10,35 +10,48 @@ public class FlamingNinjaStar {
 		int flameSize = 130;		//the length of the flaming arms
 		
 		// Make a new robot, and set it's pen down.
+		Robot pal=new Robot();
 
 		// Set the robot speed to 100
+		pal.penDown();
+		pal.setSpeed(100);
 		
 		// Set the robot window size to 800 x 800
+		pal.setWindowSize(800, 800);
 
 		//  COUNT. Make another int variable to count how many times the loop has repeated
 		//         Set its start value to zero.
+		int count=0;
 		
 	       //  LOOP. Start a while loop to repeat all of the code below ONE time (we will change this later)
-
+while(count<25) {
 			   // TURN RIGHT     Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full circle)
+	pal.turn(360/8);
 		
 			   // MOVE           Move the robot 64 pixels
+	pal.move(64);
 
 			   // TURN LEFT      Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
+	pal.turn(-40);
 			
 			   // DRAW FLAME     Move the robot the distance in the variable flameSize
+	pal.move(flameSize);
 			
 				//               Turn the robot 170 degrees
+	pal.turn(170);
 			
 				//               Move the robot the distance in the variable flameSize (again)
-			
+			pal.move(flameSize);
 			   // TURN RIGHT     Turn the robot 64 degrees to the right
+			pal.turn(64);
 			
 				// MOVE         Move the robot the distance in the variable baseSize
-		
+		pal.move(baseSize);
 				//  INCREASE COUNT. Increase the count by 1
+		count+=1;
 		
 		//  End the while loop here
+}
 		
 		// TEST   Run the program. Check that your shape is the same as the first picture in the recipe. 
 		//        This is one arm of the ninja star.
